e8b9c7712cd4ce668ac706eabac8d3e512e6baad
Peter Valdez <peter@nycmesh.net>
2019-08-28 13:30:24 +0900

    Add dynamic wishes to the wishlist page

--- frontend/src/components/Wishlist.vue
+++ frontend/src/components/Wishlist.vue
@@ -2,17 +2,60 @@
   <div class="wishlist">
     <h1>Wishlist for {{ $route.params.address }}</h1>
     <p>Submit &amp; vote on what you want this location to be!</p>
+    <div>
+      <input type="text" name="wish" id="wish" value="" />
+      <input type="submit" value="Submit" />
+    </div>
+
+    <div class="wish-container">
+      <div v-for="wish in wishes" class="card wish mr-3 mb-3">
+        <div class="card-body">
+          <h5 class="card-title">{{ wish }}</h5>
+          <p class="card-text">9 votes</p>
+          <a href="#" class="btn btn-primary">-1</a>
+          <a href="#" class="btn btn-primary">+1</a>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Wishlist',
+  data: () => ({
+    wishes: [
+      'Barbecue', 'Beer hall', 'Archery range',
+      'Barbecue', 'Beer hall', 'Archery range',
+      'Barbecue', 'Beer hall', 'Archery range',
+    ]
+  }),
   props: { address: String }
 }
 </script>
 
 <style scoped>
 h3 { margin: 40px 0 0; }
+
 a { color: #42b983; }
+
+.card-body a {
+  color: white;
+  margin: 0 10px;
+  width: 3rem;
+}
+
+.wish-container {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.wish {
+  margin-top: 5px;
+}
+
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,5 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +9,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')

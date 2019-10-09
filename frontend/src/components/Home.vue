8e4aa5c3d1dce4e798fa071ffb4435ed15d8ffb7
Peter Valdez <peter@nycmesh.net>
2019-10-08 22:46:26 -0400

    Update main page copy

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,9 +2,9 @@
   <div class="body">
     <h1 class="mb-5">Vacant Location Wishlists</h1>
     <p>Have you ever walked by a vacant store and wished it would open as a new bookstore?</p>
-    <p>Or an archery range? Or a beer hall?</p>
+    <p>Or a daycare? Or an archery range?</p>
     <p>Well, now is the time to be heard!</p>
-    <p class="mt-5"><strong>Look for our Wishlist QR stickers near vacant storefronts, scan them, and cast your vote!</strong></p>
+    <p class="mt-5"><strong>Look for wishlist QR stickers near vacant storefronts, scan them, and cast your vote!</strong></p>
   </div>
 </template>
 

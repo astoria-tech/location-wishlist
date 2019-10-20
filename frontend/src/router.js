7f20956eaf028f9cb0464ebb081aaeb74203bba6
James Quigley <james@quigley.us>
2019-10-20 09:12:51 -0400

    For consistent styling, use prettier in the whole project

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/list', name: 'list', component: () => import('./views/List.vue') },
-    { path: '/wishlist/:address(.*)', name: 'wishlist', component: () => import('./views/Wishlist.vue') }
+    { path: "/", name: "home", component: Home },
+    {
+      path: "/list",
+      name: "list",
+      component: () => import("./views/List.vue")
+    },
+    {
+      path: "/wishlist/:address(.*)",
+      name: "wishlist",
+      component: () => import("./views/Wishlist.vue")
+    }
   ]
-})
+});

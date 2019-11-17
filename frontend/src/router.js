0b512baab69f1944ffdd689dfef363b3f3b3cded
muse_c <47368784+rshieh-student@users.noreply.github.com>
2019-11-17 17:11:42 -0500

    Add NewLocation.vue page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -10,6 +10,7 @@ export default new Router({
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/list', name: 'list', component: () => import('./views/List.vue') },
-    { path: '/wishlist/:address(.*)', name: 'wishlist', component: () => import('./views/Wishlist.vue') }
+    { path: '/wishlist/:address(.*)', name: 'wishlist', component: () => import('./views/Wishlist.vue') },
+    { path: '/new-location', name: 'new-location', component: () => import('./views/NewLocation.vue') }
   ]
 })

112f8b2811df7b06c4427052fd50d773aab8d308
Omar De Leo <omar.deleo@gmail.com>
2019-11-17 17:38:16 -0500

    Create route to AdminSubmission

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import AdminSubmission from './components/AdminSubmission.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,7 @@ export default new Router({
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/list', name: 'list', component: () => import('./views/List.vue') },
-    { path: '/wishlist/:address(.*)', name: 'wishlist', component: () => import('./views/Wishlist.vue') }
+    { path: '/wishlist/:address(.*)', name: 'wishlist', component: () => import('./views/Wishlist.vue') },
+    { path: '/adminsubmission', name: 'adminsubmission', component: AdminSubmission }
   ]
 })

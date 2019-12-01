7c54025ca83592d7401f918b2596e95052570c39
Omar De Leo <omar.deleo@gmail.com>
2019-12-01 15:53:27 -0500

    Implement submit location functionality

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -6,7 +6,7 @@
     <p>Well, now is the time to be heard!</p>
 
     <div class="mt-5 mb-5">
-      <p class="text-success h3">Submit a vacant location below!</p>
+      <p class="text-success h3">Submit a vacant location below</p>
 
       <div class="container mt-3">
         <div class="row justify-content-center">
@@ -18,17 +18,48 @@
           </form>
         </div>
       </div>
+      <div class="mt-5 message-box text-success"></div>
     </div>
     <p class="mt-5"><strong>Look for wishlist QR stickers near vacant storefronts, scan them, and cast your vote!</strong></p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Home',
+  methods: {
+    handleSubmit: function() {
+      let location = document.querySelector('.form-control');
+      if (location.value) {
+        this.addLocation(location.value);
+        location.value = "";
+      }
+    },
+    addLocation: async function (address) {
+      console.log('addloc', address)
+      await axios({
+        url: '/graphql',
+        method: 'post',
+        data: {
+          query: `
+            mutation {
+              addLocation(
+                address: "${address}"
+              )
+            }
+          `
+        }
+      });
+      let messageBox = document.querySelector('.message-box');
+      messageBox.innerText = "Thank you for your entry. Check back soon :)"
+    },
+  },
   props: { msg: String }
 }
 </script>
+    
 
 <style scoped>
 </style>

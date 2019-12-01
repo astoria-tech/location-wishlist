08c1dcef226c9e76bea9440f0309dd1656fa152d
Omar De Leo <omar.deleo@gmail.com>
2019-12-01 16:21:23 -0500

    Create Submission component

---
+++ frontend/src/components/Submission.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="submission-container">
+    <h4>New Submission</h4>
+    <img class="submission-img" src="https://i.imgur.com/LF1Ndmb.jpg">
+    <div class="address">
+      <div>{{location.address}}</div>
+    </div>
+    <div class="timestamp">
+      <span>Submitted on:</span>
+      {{formatDate(location.createdAt)}}
+    </div>
+    <div class="button-container">
+      <button>Accept</button>
+      <button>Reject</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { formatDate } from '../mixins/formatDate.js'
+export default {
+  name: "Submission",
+  props: {
+    location: Object
+  }, 
+  mixins: [formatDate]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h4 {
+    margin: 3px 0;
+  }
+  .submission-container {
+    border: 1px solid gray;
+    border-radius: 15px;
+    width: 300px;
+    padding-bottom: 5px;
+  }
+  .submission-img {
+    width: 300px;
+    height: 200px;
+    object-fit: cover;
+  }
+  .address {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .timestamp {
+    font-size: 11px;
+    margin-bottom: 5px;
+  }
+  .timestamp span {
+    font-weight: 600;
+  }
+</style>

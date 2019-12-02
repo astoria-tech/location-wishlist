f1bcb9e609050e2f2b6ae8e653c98750406588fd
Omar De Leo <omar.deleo@gmail.com>
2019-12-01 20:13:05 -0500

    Add approve/reject location functionality

--- frontend/src/components/Submission.vue
+++ frontend/src/components/Submission.vue
@@ -10,49 +10,84 @@
       {{formatDate(location.createdAt)}}
     </div>
     <div class="button-container">
-      <button>Accept</button>
-      <button>Reject</button>
+      <a v-on:click="approveLocation(location.address)" href="#" class="btn btn-outline-success">Approve</a>
+      <a v-on:click="rejectLocation(location.address)" href="#" class="btn btn-outline-success">Reject</a>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 import { formatDate } from '../mixins/formatDate.js'
+
 export default {
   name: "Submission",
   props: {
     location: Object
   }, 
-  mixins: [formatDate]
+  mixins: [formatDate],
+  methods: {
+    approveLocation: async function (address) {
+      await axios({
+        url: '/graphql',
+        method: 'post',
+        data: {
+          query: `
+            mutation {
+              approveLocation(
+                address: "${address}"
+              )
+            }
+          `
+        }
+      });
+    },
+    rejectLocation: async function (address) {
+      await axios({
+        url: '/graphql',
+        method: 'post',
+        data: {
+          query: `
+            mutation {
+              rejectLocation(
+                address: "${address}"
+              )
+            }
+          `
+        }
+      });
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h4 {
-    margin: 3px 0;
-  }
-  .submission-container {
-    border: 1px solid gray;
-    border-radius: 15px;
-    width: 300px;
-    padding-bottom: 5px;
-  }
-  .submission-img {
-    width: 300px;
-    height: 200px;
-    object-fit: cover;
-  }
-  .address {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .timestamp {
-    font-size: 11px;
-    margin-bottom: 5px;
-  }
-  .timestamp span {
-    font-weight: 600;
-  }
+h4 {
+  margin: 3px 0;
+}
+.submission-container {
+  border: 1px solid gray;
+  border-radius: 15px;
+  width: 300px;
+  padding-bottom: 5px;
+}
+.submission-img {
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+}
+.address {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.timestamp {
+  font-size: 11px;
+  margin-bottom: 5px;
+}
+.timestamp span {
+  font-weight: 600;
+}
 </style>

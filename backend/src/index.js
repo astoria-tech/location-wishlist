f8468cae281480e8ad30da4f2633dc163b2e39e9
Blane <blane.cordes@gmail.com>
2019-12-15 21:06:30 -0500

    fix creating suggestions

--- backend/src/index.js
+++ backend/src/index.js
@@ -107,11 +107,11 @@ const resolvers = {
             return models.Suggestion.findOrCreate({
               where: {
                 idea,
-                locationId: location.id
+                LocationId: location.id
               },
               defaults: {
                 idea,
-                locationId: location.id,
+                LocationId: location.id,
                 votes: 0
               }
             });

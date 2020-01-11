10f9cd8120bcdd6de636929d6c5c2f97e865dc61
Omar De Leo <omar.deleo@gmail.com>
2020-01-10 19:13:28 -0500

    Create user model

---
+++ backend/src/models/user.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [7, 42],
+      },
+    }
+  }, {});
+
+  User.beforeCreate(async user => {
+    user.password = await user.generatePasswordHash();
+  });
+
+  User.prototype.generatePasswordHash = async function() {
+    const saltRounds = 10;
+    return await bcrypt.hash(this.password, saltRounds);
+  };
+
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+
+  return User;
+};

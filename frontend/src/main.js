338e650488078b595e2a8a7be5c92ef6ac1f0ef8
Omar De Leo <omar.deleo@gmail.com>
2020-01-13 15:53:41 -0500

    Install vue-apollo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
